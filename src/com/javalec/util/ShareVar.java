4afa6206b6d110c078c1b146c57c365e6b23c2c0
Donghyun <pdchoirian@gmail.com>
2023-05-10 12:35:03 +0900

    Project 기본 틀 생성, classpath 설정, JDBC 라이브러리 연결
    프로젝트 내에 lib 파일을 배치하여 공용으로 사용할 수 있게 구현

---
+++ src/com/javalec/util/ShareVar.java
@@ -0,0 +1,16 @@
+package com.javalec.util;
+
+public class ShareVar {
+
+
+//		public static final String DBName = "jdbc:mysql://127.0.0.1/sunshine?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+		/* 팀장 IP */
+		public static final String DBName = "jdbc:mysql://192.168.50.7/sunshine?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+		/* DB 계정 */
+		public static final String DBUser = "root";
+		/* DB 비밀번호 */
+		public static final String DBPass = "qwer1234";
+		/* File */
+		public static int filename = 0;
+	
+}
