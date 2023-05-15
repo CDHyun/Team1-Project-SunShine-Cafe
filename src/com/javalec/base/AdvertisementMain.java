ddb8cc177dd2f1d5a0bf8e2d151aee4c1e9501ce
Donghyun <pdchoirian@gmail.com>
2023-05-15 10:15:05 +0900

    ShareVar IP 변경, 광고 화면 시간 지나면 바뀌게 수정

--- src/com/javalec/base/AdvertisementMain.java
+++ src/com/javalec/base/AdvertisementMain.java
@@ -5,10 +5,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.javalec.function.ImageResize;
+
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class AdvertisementMain extends JFrame {
 
@@ -36,6 +41,12 @@ public class AdvertisementMain extends JFrame {
 	 * Create the frame.
 	 */
 	public AdvertisementMain() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				changeAd();
+			}
+		});
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 625, 900);
 		contentPane = new JPanel();
@@ -63,4 +74,30 @@ public class AdvertisementMain extends JFrame {
 		}
 		return lblAD;
 	}
-}
+	
+	private void changeAd() {
+		long startTime = System.currentTimeMillis();
+
+        while (System.currentTimeMillis() - startTime < 5000) {
+        }
+        ImageIcon icon = new ImageIcon(AdvertisementMain.class.getResource("/com/javalec/image/Ad2.png"));
+        int x = 625;
+        int y = 900;
+        ImageResize resize = new ImageResize(icon, x, y);
+        ImageIcon ad2 = resize.imageResizing();
+        lblAD.setIcon(ad2);
+        
+        while (System.currentTimeMillis() - startTime < 5000) {
+        }
+        icon = new ImageIcon(AdvertisementMain.class.getResource("/com/javalec/image/Ad2.png"));
+        resize = new ImageResize(icon, x, y);
+        ImageIcon ad3 = resize.imageResizing();
+        lblAD.setIcon(ad3);
+        
+        
+        
+	}
+	
+	
+	
+}	// End Class
